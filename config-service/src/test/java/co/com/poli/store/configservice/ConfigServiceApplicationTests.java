ccc6c3c749f0e5f17565c4f2f8de54ac894ddc97
Giovanny Vargas <yovaz05@gmail.com>
2022-05-09 19:58:33 -0500

    clase 09-05-2022

---
+++ config-service/src/test/java/co/com/poli/store/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.store.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
