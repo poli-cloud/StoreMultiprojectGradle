61d767b6ef719a1b5912aaffbbda6ae00bfc80d6
Giovanny Vargas <yovaz05@gmail.com>
2022-07-09 16:22:13 -0500

    clase 09-07-2022

---
+++ admin-service/src/test/java/co/com/poli/store/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.store.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
